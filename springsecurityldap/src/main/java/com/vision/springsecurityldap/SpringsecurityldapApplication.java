5132a9f49dd4435c8ee126622a38c39646f956a9
M1052905 <M1052905@MT-PF1KA1UB>
2021-03-02 13:45:48 +0530

    first commit

---
+++ springsecurityldap/src/main/java/com/vision/springsecurityldap/SpringsecurityldapApplication.java
@@ -0,0 +1,13 @@
+package com.vision.springsecurityldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityldapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityldapApplication.class, args);
+	}
+
+}
---
+++ springsecurityldap/src/test/java/com/vision/springsecurityldap/SpringsecurityldapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vision.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityldapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
